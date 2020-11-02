94b3ea7b256ddc6ca989efc37def826482166c9c
davidreke <54542172+davidreke@users.noreply.github.com>
2020-11-01 22:40:38 -0600

    finish redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
 import AppNavbar from './components/AppNavbar'
 import ShoppingList from './components/ShoppingList'
+import ItemModal from './components/itemModal'
 
 import { Provider} from  'react-redux';
 import store from './store'
 
+import {Container} from 'reactstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
@@ -12,10 +14,13 @@ class App extends Component {
   render () {
     return (
       <Provider store={store}>
-        <div className="app">
-          <AppNavbar />
-          <ShoppingList />
-        </div>
+        <Container>
+          <div className="app">
+            <AppNavbar />
+            <ItemModal />
+            <ShoppingList />
+          </div>
+        </Container>
       </Provider>
     )
   }
