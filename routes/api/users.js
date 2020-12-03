1219d6b017a29507d7b04b7961f2460c2042a7df
davidreke <54542172+davidreke@users.noreply.github.com>
2020-12-03 14:42:38 -0600

    fix user model relative path

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,7 +5,7 @@ const config = require('config')
 const jwt = require('jsonwebtoken')
 
 // USer Model
-const User = require('../../models/User');
+const User = require('../../models/user');
 
 // @route POST api/users
 // @desc Register new user
